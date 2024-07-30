9e9bb21719b290e3f69b2956849510b921ffefc8
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-07-30 13:26:00 +0530

    Write logic to get computer choice
    Choice is based on the random numbers generated

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello!");
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors"
+
+function getComputerChoice() {
+    // Get a number that is between 0 (inclusive) and 3(exclusive)
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber == 0) {
+        return ROCK;
+    } else if (randomNumber == 1) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
