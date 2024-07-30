da6912c2a36c168c7bbd9c49935097154a732d17
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-07-30 19:19:20 +0530

    Write logic to track scores and play a round
    Find and display the round winner and increment the score

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const ROCK = "rock";
-const PAPER = "paper";
-const SCISSORS = "scissors"
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     // Get a number that is between 0 (inclusive) and 3(exclusive)
@@ -31,5 +34,25 @@ function getPlayerChoice() {
 }
 
 function formatPlayerChoice(playerChoice) { 
-    return playerChoice.trim().toLowerCase();
+    return playerChoice.trim().toUpperCase();
+}
+
+function playRound(playerChoice, computerChoice) {
+    if ((playerChoice === ROCK && computerChoice === SCISSORS)
+    || (playerChoice === PAPER && computerChoice === ROCK)
+    || (playerChoice === SCISSORS && computerChoice === PAPER)
+    ) {
+        // Player has won
+        // Increment player's score and display winning message
+        playerScore++;
+        console.log(`YOU WON! ${playerChoice} beats ${computerChoice}`);
+    } else if (playerChoice === computerChoice) { 
+        // Tie
+        console.log("TIE!");
+    } else {  
+        // Player has lost
+        // Increment computer's score and display losing message
+        computerScore++;
+        console.log(`YOU LOSE! ${computerChoice} beats ${playerChoice}`);
+    }
 }
